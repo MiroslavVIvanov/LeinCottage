31971d1a4c3143f18ddd1c043d7731e7fb4086f1
Miroslav Ivanov <miroslav.ivanov@hotmail.com>
2017-01-25 16:02:34 +0200

    Added Photo model

---
+++ LeinCottage/Data/LeinCottage.Data/LeinCottageDbContext.cs
@@ -0,0 +1,9 @@
+﻿namespace LeinCottage.Data
+{
+    using Models;
+    using System.Data.Entity;
+    public class LeinCottageDbContext: DbContext
+    {
+        public virtual IDbSet<Photo> Photos { get; set; }
+    }
+}
---
+++ LeinCottage/Data/LeinCottage.Models/Photo.cs
@@ -0,0 +1,22 @@
+﻿namespace LeinCottage.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Photo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string OriginalName { get; set; }
+
+        public string Name { get; set; }
+
+        public string RelativePath { get; set; }
+
+        public string ThumbnailName { get; set; }
+
+        public string ThumbnailRelativePath { get; set; }
+
+        public bool IsVisible { get; set; }
+    }
+}
