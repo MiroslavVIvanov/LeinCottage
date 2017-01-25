57138486491e9175c0ef23665e3ffe8e4339fcc8
Miroslav Ivanov <miroslav.ivanov@hotmail.com>
2017-01-25 15:34:56 +0200

    Included BasicAuthenticationAttribute and view&controller for administrating gallery photos.

---
+++ LeinCottage/Web/LeinCottage.Web/Controllers/ImagesAdministrationController.cs
@@ -0,0 +1,14 @@
+﻿namespace LeinCottage.Web.Controllers
+{
+    using CustomAttributes;
+    using System.Web.Mvc;
+
+    public class ImagesAdministrationController : Controller
+    {
+        [BasicAuthentication("", "")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
